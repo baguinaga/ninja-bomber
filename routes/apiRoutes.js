42b5fe4b44064485669c693167aaa9f79e65ed89
baguinaga <42448094+Baguinaga@users.noreply.github.com>
2018-10-31 09:57:58 -0400

    cleaned up file structure

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,14 @@ const db = require("../models");
 module.exports = function(app) {
   // Get all users
   app.get("/api/users", function(req, res) {
-    db.User.findAll({}).then(function(users) {
+    db.user.findAll({}).then(function(users) {
       res.json(users);
     });
   });
 
   // Create a new user
   app.post("/api/users", function(req, res) {
-    db.User.create(req.body).then(function(users) {
-      res.json(users);
-    });
-  });
-
-  // Delete a user by id
-  app.delete("/api/users/:id", function(req, res) {
-    db.User.destroy({ where: { id: req.params.id } }).then(function(users) {
+    db.user.create(req.body).then(function(users) {
       res.json(users);
     });
   });
